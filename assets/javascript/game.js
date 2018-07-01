5738c848b5008f4749db2cdd9e26dbcfc3b714b7
David Penberthy <davidpenberthy47@gmail.com>
2018-07-01 15:06:23 -0500

    created file structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+//link jQuery to the DOM 
+
+// put each character into an object with characteristics
+
+    //build functionality for two moves per pokemon
+
+        //two moves with unique abilities
+    
+        //chance of missing
+
+        //HP
+
+        //HP goes down when hit
+
+        //pokemon faints when it runs out of HP
+
+// ability to select a pokemon
+
+    // when i pick it i am able to control the pokemon
+
+    // put functions for the pokemons moves in here
+
+//computer randomly selects an opponent pokemon
+
+    //moves the enemy character into the defender
+
+    //computer randomly selects a move
+
+    // BONUS::: once you've knocked out an opponent, you will get a random prize
+
+    // you must select your next opponent after you've defeated your opponent
+
+//press the keys via the buttons to use an attack
+
+    //the attack buttons will appear when you choose a pokemon
+
+    //if no defender pokemon is choosen, a document.write with "no enemy selected" will appear.
+
+
+//when your HP is reduced to 0, the game is over. 
+
+//you win once you've knocked out every opponent
+
+//when 
+
+//press restart to restart the game
